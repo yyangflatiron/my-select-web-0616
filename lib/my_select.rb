969740ab15b8adbbae48ea52da423240909aaf5f
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-25 14:57:18 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  even_num = []
+  while i < collection.length
+    if yield(collection[i])
+      even_num << collection[i] 
+    end
+    i+=1
+  end
+  even_num
 end
+
